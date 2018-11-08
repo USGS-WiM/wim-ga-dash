0f4d31fd203081faae1cad4480d18c71435977e2
Rachel Stevenson-USGS <rstevenson@usgs.gov>
2018-11-08 11:00:22 -0700

    Added View IDs
    Array of View Ids  for Dashboard

---
+++ View-Ids.js
@@ -0,0 +1,143 @@
+var properties = [
+    {
+      'name': 'CBRS Mapper V2',
+      'view_id': '153916108'
+    },
+    {
+      'name': 'CBRS Projects Mapper',
+      'view_id': '168297416'
+    },
+    {
+      'name': 'FEV',
+      'view_id': '131080146'
+    },
+    {
+      'name':'FIM',
+      'view_id': '116842326'
+    },
+    {
+      'name': 'FIM-JS',
+      'view_id':'147316934'
+    },
+    {
+      'name': 'Fishtail', 
+      'view_id': '123459504'
+    },
+    {
+      'name':'FishVis',
+      'view_id':'123559590'
+    },
+    {
+      'name': 'GLCA Landing Page',
+      'view_id': '137384842'
+    },
+    {
+      'name': 'GLCWRA/CRSRA', 
+      'view_id': '137355369'
+    },
+    {
+      'name': 'GLCWRA/GBRA',
+      'view_id': '164819069'
+    }, 
+    {
+      'name': 'GLCWRA/SBRA',
+      'view_id': '137369161'
+    },
+    {
+      'name': 'GLCWRA/WLERA',
+      'view_id':'136538015'
+    }, 
+    {
+      'name': 'GroundWaterWatch',
+      'view_id': '129874603'
+    },
+    {
+      'name':'Krigservices',
+      'view_id':'172791801'
+    },
+    {
+      'name':'MARB2015',
+      'view_id':'116067724'
+    },
+    {
+      'name':'MercuryMapper', 
+      'view_id':'142693416'
+    }, 
+    {
+      'name':'NaWQA Decadal Trends Mapper',
+      'view_id':'116129201', 
+    },
+    {
+      'name': 'NAWQA SW Trends',
+      'view_id': '139280803'
+    }, 
+    {
+       'name':'NWIS Mapper',
+       'view_id':'120202187'   
+    },
+    {
+      'name':'NWIS Mapper (WaterAlert)',
+      'view_id':'120793561'
+    },
+    {
+      'name':'Phragmites',
+      'view_id':'167897616'
+    }, 
+    {
+      'name': 'SIGL',
+      'view_id':'166921307'
+    },
+    {
+      'name':'Sparrow- East-US',
+      'view_id':'172461123'   
+    },
+    {
+       'name':'Sparrow Great Lakes',
+       'view_id':'166992080'   
+    }, 
+    {
+       'name':'Sparrow-MARB',
+       'view_id':'172288143'    
+    },
+    {
+      'name':'Sparrow-Midcontinental',
+      'view_id':'181503928'
+    },
+    {
+      'name':'Sparrow National Dissolved Solids',
+      'view_id':'181595027'
+    },
+    {
+      'name':'Sparrow Pacfic Northwest',
+      'view_id':'181590692'
+    },
+    {
+      'name':'Sparrow-Tennesee',
+      'view_id':'141180663'
+    },
+    {
+      'name':'Stnweb',
+      'view_id':'161903487'
+    },
+    {
+      'name':'StreamStats',
+      'view_id':'116153556'
+    },
+    {
+      'name':'StreamStatsNavigation',
+      'view_id':'162976293'
+    },
+    {
+      'name':'StreamstatsWateruseServices',
+      'view_id':'164581924'
+    },
+    {
+      'name':'Wetlands-projects',
+      'view_id':'70971957'
+    },
+    {
+      'name':'WIM Main',
+      'view_id':'99989301'
+    }
+
+  ];
